42cc39cbc8639869e975b6f24de816787dcc43de
Glitch (principled-emu) <none>
2019-12-31 04:06:40 +0000

    🐂🏊🏼 Checkpoint ./myApp.js:9674960/151

--- myApp.js
+++ myApp.js
@@ -75,7 +75,13 @@ app.get('/:word/echo', (req, res) => {
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.get('/name', (req, res) => {
+  var { first: firstName, last: lastName } = req.query;
+  
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
